e70d6901f5ac922fa77890864cf74192741bd000
bdiniscia <bdiniscia@gmail.com>
2020-06-27 21:32:51 -0400

    Done the styles of the card posts

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
+import NewsCard from './NewsCard';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='homeContainer'>
+            <div className='sorter'>
+                <div>
+                    <span>All</span>
+                    <span>My Faves</span>
+                </div>
+            </div>
+            <div className='postsContainer'>
+                <NewsCard
+                time='2 hours'
+                author='author'
+                title='Event-driven state management in React using Storeon' 
+                liked = {true} />
+                <NewsCard
+                time='3 hours'
+                author='James John'
+                title='All the fundamental React.js concepts, jammed into this single Medium article (updated August 2019)' 
+                liked = {false} />
+                <NewsCard
+                time='4 hours'
+                author='author'
+                title='Yes, React is taking over front-end development. The question is why.' 
+                liked = {true} />
+            </div>
         </div>
     )
 }
--- src/Components/NewsCard.jsx
+++ src/Components/NewsCard.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './NewsCard.css';
 
-const NewsCard = () => {
+const NewsCard = ({time, author, title, liked }) => {
     return (
-        <div>
-            
+        <div className='rectangle'>
+            <div className='contentInfo'> 
+                <div className='createdDiv'>
+                    <img alt='icon for the posted time' className='iconClock' src={require('../img/iconmonstr-time-2.svg')} />
+                    <span>{time} by {author}</span>
+                </div>
+                <h3 className='titlePost'>{title}</h3>
+            </div>
+            <div className='likedDiv'>
+                { liked ?
+                <img alt='liked' src={require('../img/iconmonstr-favorite-3.svg')} />
+                :
+                <img alt='not liked' src={require('../img/iconmonstr-favorite-2.svg')} />
+                }
+            </div>
         </div>
     )
 }
